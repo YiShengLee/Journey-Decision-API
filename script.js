7e567f6cb31f31272c7f3619f1f0cdb37db95600
YSLee <54732188+YiShengLee@users.noreply.github.com>
2019-11-25 21:17:25 +0800

    Added map css

--- script.js
+++ script.js
@@ -17,30 +17,34 @@ $(document).ready(function () {
     // testing if the preventDefault working
     // console.log("form...");
     var location = $("#location").val();
-    
+
 
     // var url = "https://maps.googleapis.com/maps/api/geocode/json?address=+" + location + ",+SG&key=AIzaSyCHWMwcTeozXv6qrb4iD6l5JRZZ9HFeSa4";
-    $.ajax({url: url, success: function(result){
-      // console.log(result["results"][0]["geometry"]["location"]);
-      // console.log(google.maps.GeocoderStatus);
-      // var newmap = result["results"][0]["geometry"]["location"];
+    $.ajax({
+      url: url,
+      success: function (result) {
+        // console.log(result["results"][0]["geometry"]["location"]);
+        // console.log(google.maps.GeocoderStatus);
+        // var newmap = result["results"][0]["geometry"]["location"];
 
-    if (google.maps.GeocoderStatus.OK == "OK"){
-      var newmap = result["results"][0]["geometry"]["location"];
-            var marker = new google.maps.Marker({
-        position: newmap,
-        map: map
-        // zoom: 20
+        if (google.maps.GeocoderStatus.OK == "OK") {
+          var newmap = result["results"][0]["geometry"]["location"];
+          var marker = new google.maps.Marker({
+            position: newmap,
+            map: map
+            // zoom: 20
+          });
+
+          // Set a marker on the search location
+          map.setCenter(newmap);
+          map.setZoom(18);
+          // map.panTo(marker.position);
+        } else {
+          alert('Geocode was not successful for the following reason: ' + google.maps.GeocoderStatus.OK);
+        };
+
+      }
     });
-    map.setCenter(newmap);
-    map.setZoom(18);
-    // map.panTo(marker.position);
-    }
-    else{
-      alert('Geocode was not successful for the following reason: ' + google.maps.GeocoderStatus.OK);
-    };
-
-    }});
 
 
 
@@ -48,7 +52,56 @@ $(document).ready(function () {
 
 });
 
+// $.ajax({
+//   type: "GET",
+//   datatype: "json",
+//   url:"https://ipapi.co/8.8.8.8/json/",
 
-$( "#toggle1" ).click(function() {
-  $( "#map" ).toggle();
+//   success: function (data) {
+//     console.log(data);
+//   }
+
+// });
+var latitudeCurrent;
+var longtitudeCurrent;
+
+// Ipappi Section 
+$.getJSON('https://ipapi.co/json/', function (data) {
+  // console.log(data);
+  var country = data.city;
+  latitudeCurrent = data.latitude;
+  longtitudeCurrent = data.longitude;
+  $("#country").html(country)
+  weather()
+  // console.log("lat" + latitudeCurrent);
+  // console.log("long" + longtitudeCurrent);
+});
+
+
+// // Weather API
+// function weather() {
+//   var settings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "dataType": "jsonp",
+//     // "url": "https://api.darksky.net/forecast/795c3669281b12e43538aa2100be89fb/" + lat + "," + long ,
+//     "url": "https://api.darksky.net/forecast/795c3669281b12e43538aa2100be89fb/" + latitudeCurrent + "," + longtitudeCurrent,
+//     "method": "GET"
+//   }
+
+//   $.ajax(settings).done(function (forecast) {
+//     var summary = forecast["currently"]["summary"];
+//     console.log(summary);
+//     $("#currSummary").html(summary);
+//     // console.log(response);
+//   });
+// }
+
+
+
+// Toggle Section
+
+// Toggle Section Button
+$("#toggle1").click(function () {
+  $("#map").toggle();
 });
